1834af408fab51dc754a75ca385052c7f88891cd
Gleb <gkmrm98@gmail.com>
2023-02-26 18:54:09 +0400

    Update Card Component

---
+++ src/components/Card/Card.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+
+import getFormatDate from '@utils/getFormatDate';
+
+import styles from './Card.module.scss';
+import star from './Star.svg';
+
+export type CardProps = {
+  image: string;
+  title: string;
+  owner: string;
+  htmlLink: string;
+  updated: string;
+  starCount: number;
+  onClick?: React.MouseEventHandler;
+};
+
+export const Card: React.FC<CardProps> = ({ image, title, owner, htmlLink, updated, starCount, onClick }) => {
+  return (
+    <div className={styles['gitRepoTile']} onClick={onClick}>
+      <img width={80} height={80} className={styles['avatar']} src={image} alt="Repository Avatar" />
+      <div className={styles['description']}>
+        <div className={styles['title']}>{title}</div>
+        <a className={styles['link']} href={htmlLink}>
+          {owner}
+        </a>
+
+        <div className={styles['info']}>
+          <div>
+            <span className={styles['star']}>
+              <img src={star} alt="Star Icon" /> <span className={styles['star_count']}>{starCount}</span>
+            </span>
+          </div>
+          <div className={styles['date']}>
+            Updated &nbsp;
+            {getFormatDate(updated)}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};

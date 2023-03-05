c18144a5a4bbcac78dc64cbb42074b3eab08dba0
Gleb <gkmrm98@gmail.com>
2023-03-05 20:32:01 +0400

    NoCommit changes

--- src/config/urlsCreator.ts
+++ src/config/urlsCreator.ts
@@ -4,7 +4,8 @@ const createUrl = (url: string): string => `${BASE_URL}${url}`;
 
 const urls = {
   repos: (params: { owner: string; name: string }): string => createUrl(`/repos/${params.owner}/${params.name}`),
-  orgs: (params: string): string => createUrl(`/orgs/${params}/repos?page=`),
+  orgs: (params: { org: string; type: string }): string =>
+    createUrl(`/orgs/${params.org}/repos?&type=${params.type}&page=`),
   readme: (params: { owner: string; name: string }): string =>
     createUrl(`/repos/${params.owner}/${params.name}/readme`),
 };

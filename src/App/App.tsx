4010e7a2af3dee05f4f120f6d09dd70e444d69c6
Gleb <gkmrm98@gmail.com>
2023-03-05 20:35:11 +0400

    Change routing

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,20 +1,14 @@
-import React from 'react';
-
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
-
-import { RepoPage } from './pages/RepoPage';
-import { Repositories } from './pages/Repositories';
+import RepoPage from '@pages/RepoPage';
+import Repositories from '@pages/Repositories';
+import { Routes, Route, Navigate } from 'react-router-dom';
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Repositories />} />
-        <Route path="/repo/:owner" element={<Repositories />} />
-        <Route path="/repo/:owner/:name" element={<RepoPage />} />
-        <Route path="*" element={<Navigate to="/" replace />} />
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Repositories />} />
+      <Route path="/repo/:owner/:name" element={<RepoPage />} />
+      <Route path="*" element={<Navigate to="/" replace />} />
+    </Routes>
   );
 };
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
 
 import App from '@App/App';
+import '@config/configureMobX';
 import ReactDOM from 'react-dom/client';
-
+import { BrowserRouter } from 'react-router-dom';
 import './styles/style.scss';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
-  <React.StrictMode>
+  <BrowserRouter>
     <App />
-  </React.StrictMode>
+  </BrowserRouter>
 );

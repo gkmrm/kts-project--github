45b3981331eb28996f9e202ec5f567e4c5b8dce6
Gleb <gkmrm98@gmail.com>
2023-03-10 12:16:26 +0400

    mini fixes

--- src/utils/debounce.ts
+++
@@ -1,9 +0,0 @@
-export function debounce<T extends any[]>(f: (...args: T) => any, timeMs: number): (...args: T) => void {
-  let timeout: NodeJS.Timeout;
-  return (...args: T) => {
-    clearTimeout(timeout);
-    timeout = setTimeout(() => {
-      f(...args);
-    }, timeMs);
-  };
-}

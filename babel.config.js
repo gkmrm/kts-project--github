492e3eabec7a581b0b73720e1393dcde65dcc07a
Gleb <gkmrm98@gmail.com>
2023-03-12 13:13:29 +0400

    Add babel.config

---
+++ babel.config.js
@@ -0,0 +1,15 @@
+module.exports = function (api) {
+  api.cache.using(() => process.env.NODE_ENV);
+
+  const presets = ['@babel/preset-env', '@babel/preset-react', '@babel/preset-typescript', 'mobx'];
+  const plugins = [
+    process.env.NODE_ENV === 'development' && 'react-refresh/babel',
+    '@babel/plugin-proposal-optional-chaining',
+    '@babel/plugin-proposal-class-properties',
+  ].filter(Boolean);
+
+  return {
+    presets,
+    plugins,
+  };
+};
